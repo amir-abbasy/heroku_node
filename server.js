b44268806cc6a7d3381b72691c7a7ebf448ac337
amir-abbasy <87001985+amir-abbasy@users.noreply.github.com>
2021-11-17 10:06:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const pusherClient = new Pusher(pusherConfig);
 const app = express(); // (2)
 app.use(bodyParser.json());
 
+const PORT = process.env.PORT || 4000;
+
 app.get('/', function (req, res) {
     res.send("HEROKU DEPLOY APP")
     console.log("WORKS");
@@ -39,6 +41,6 @@ app.post('/users/:name/messages', function (req, res) { // (5)
     res.sendStatus(204);
 });
 
-app.listen(8080, function () { // (6)
+app.listen(PORT, function () { // (6)
     console.log('App listening on port 4000');
 });
